eb6f01b0a1436a396180032c68acdc8cd6fa84eb
Jeffrey Ng <jeffreymng@netzero.net>
2016-08-13 14:24:25 -0400

    def create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
-  @user = User.find_or_create_from_auth_hash(auth_hash)
-  session[:user_id] = @user.id
-  redirect_to root_path
-
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+  
   protected
 
   def auth_hash
