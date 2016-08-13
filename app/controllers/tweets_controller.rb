c9308e6b22f0e08785e2797a002aeefaa6634fea
Jeffrey Ng <jeffreymng@netzero.net>
2016-08-13 18:11:44 -0400

    changed params user to sessions user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
     image.google_image_search
     image.get_clarifai_access_token
     image.clarifai_call
-    tweets = get_tweets(params[:user])
+    tweets = get_tweets(sessions[:user])
     post = generate_tweet(tweets)
     new_post = post.concat(" ##{image.clarifai_tags.sample}")
     @user.twitter.update_with_media(new_post, open(image.image_url))
